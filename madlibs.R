d924dc4dd3430d3bc39e061255b93f835faa7f1b
Hadley Wickham <h.wickham@gmail.com>
2024-08-02 16:30:55 -0500

    Add madlibs.R

---
+++ madlibs.R
@@ -0,0 +1,40 @@
+library(shiny)
+
+ generate_story <- function(noun, verb, adjective, adverb) {
+   paste0(
+     "Once upon a time, there was a ", adjective, " ", noun,
+      " who loved to ", verb, " ", adverb, ". It was the funniest thing ever!"
+   )
+ }
+
+ ui <- fluidPage(
+   titlePanel("Mad Libs Game"),
+
+   sidebarLayout(
+     sidebarPanel(
+       textInput("noun1", "Enter a noun:", ""),
+       textInput("verb", "Enter a verb:", ""),
+       textInput("adjective", "Enter an adjective:", ""),
+       textInput("adverb", "Enter an adverb:", ""),
+       actionButton("submit", "Create Story")
+     ),
+
+     mainPanel(
+       h3("Your Mad Libs Story:"),
+       textOutput("story")
+     )
+   )
+ )
+
+ server <- function(input, output) {
+
+   story <- eventReactive(input$submit, {
+     generate_story(input$noun1, input$verb, input$adjective, input$adverb)
+   })
+
+   output$story <- renderText({
+     story()
+   })
+ }
+
+ shinyApp(ui = ui, server = server)
