f007d3e34cd0140f294e484894dce77ea1c89dc1
Hadley Wickham <h.wickham@gmail.com>
2024-08-07 12:47:42 -0500

    Better tidy style

--- madlibs.R
+++ madlibs.R
@@ -2,9 +2,9 @@ library(shiny)
 
 generate_story <- function(noun, verb, adjective, adverb) {
   glue::glue("
-    Once upon a time, there was a {adjective} {noun} who loved to 
-    {verb} {adverb}. It was the funniest thing ever!"
-  )
+    Once upon a time, there was a {adjective} {noun} who loved to
+    {verb} {adverb}. It was the funniest thing ever!
+  ")
 }
 
 ui <- fluidPage(
